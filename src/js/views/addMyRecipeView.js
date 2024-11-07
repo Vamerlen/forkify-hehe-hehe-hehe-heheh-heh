8c34544b18616fdd03998bedaf043eaa897995ba
Vamerlen <kongolovafadie@gmail.com>
2024-11-07 18:53:53 +0200

    Update addMyRecipeView.js

--- src/js/views/addMyRecipeView.js
+++ src/js/views/addMyRecipeView.js
@@ -1,40 +1,194 @@
-import { view } from './view';
+import { View } from './View.js';
 import icons from 'url:../../img/icons.svg';
 
-const addMyRecipeView = function () {
-  view.call(this);
-  this._parentEle = document.querySelector('.upload');
-  this.windowAddRecipe = document.querySelector('.add-recipe-window');
-  this.overlay = document.querySelector('.overlay');
-  this.btnOpen = document.querySelector('.nav__btn--add-recipe');
-  this.btnClose = document.querySelector('.btn--close-modal');
-};
-addMyRecipeView.prototype = Object.create(view.prototype);
+class AddRecipeView extends View {
+  _parentEl = document.querySelector('.upload');
+  _successMessage = 'Congratulations! Your recipe was successfully uploaded.';
+  ings = ['ing-1'];
+  _formRender = false;
 
-addMyRecipeView.prototype.toggleWindow = function () {
-  this.windowAddRecipe.classList.toggle('hidden');
-  this.overlay.classList.toggle('hidden');
-};
+  _window = document.querySelector('.add-recipe-window');
+  _overlay = document.querySelector('.overlay');
+  _btnOpen = document.querySelector('.nav__btn--add-recipe');
+  _btnClose = document.querySelector('.btn--close-modal');
 
-addMyRecipeView.prototype.addHandlerShowWindow = function () {
-  this.btnOpen.addEventListener('click', () => this.toggleWindow());
-};
+  constructor() {
+    super();
+    this._addHandlerShowWindow();
+    this._addHandlerHideWindow();
+  }
 
-addMyRecipeView.prototype.addHandlerCloseWindow = function () {
-  this.btnClose.addEventListener('click', () => this.toggleWindow());
-  this.overlay.addEventListener('click', () => this.toggleWindow());
-};
+  // Handler method for uploading the recipe
+  addHandlerUpload(handler) {
+    this._parentEl.addEventListener('submit', function (e) {
+      e.preventDefault();
+      const dataArr = [...new FormData(this)];
+      const data = Object.fromEntries(dataArr);
+      handler(data);
+    });
+  }
 
-addMyRecipeView.prototype.addHandlerUpload = function (handler) {
-  this._parentEle.addEventListener('submit', event => {
-    event.preventDefault();
-    const dataArray = [...new FormData(this._parentEle)];
-    // ` Object.fromEntries ` convert the array to object
-    const data = Object.fromEntries(dataArray);
-    handler(data);
-  });
-};
+  // If form is not rendered, renders the form and displays the window
+  showWindow() {
+    if (!this._formRender)
+      this._parentEl.insertAdjacentHTML('afterbegin', this._generateMarkup());
+    this._formRender = true;
 
-addMyRecipeView.prototype.generateMarkup = function () {};
+    this._window.classList.remove('hidden');
+    this._overlay.classList.remove('hidden');
+  }
 
-export default new addMyRecipeView();
+  // Hides the form window, does not clear the form
+  hideWindow() {
+    this._window.classList.add('hidden');
+    this._overlay.classList.add('hidden');
+  }
+
+  // Hides (kills) the window and clears the form
+  killWindow() {
+    this.hideWindow();
+
+    this.ings = ['ing-1'];
+    this._clear();
+    this._formRender = false;
+  }
+
+  // Handler method to display the window
+  _addHandlerShowWindow() {
+    this._btnOpen.addEventListener('click', this.showWindow.bind(this));
+    this.ings = ['ing-1'];
+  }
+
+  // Handler method to hide/kill the window
+  _addHandlerHideWindow() {
+    this._btnClose.addEventListener('click', this.killWindow.bind(this));
+
+    this._overlay.addEventListener('click', this.hideWindow.bind(this));
+  }
+
+  // Adds an ingredient input field to the Dom
+  addIngredient() {
+    const markup = this._addIngMarkup();
+    if (this.ings.length === 10) return;
+    this.ings.push(`ing-${this.ings.length + 1}`);
+    this._parentEl
+      .querySelector('.upload__column')
+      .insertAdjacentHTML('beforeend', markup);
+  }
+
+  // Handler method to add an ingredinet input field to the Dom
+  addHandlerAddIng(handler) {
+    this._parentEl.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn_add');
+      if (!btn) return;
+      handler();
+    });
+  }
+
+  // Removes an ingredinet input field from the Dom
+  removeIngredient(lastChild) {
+    this._parentEl.querySelector('.upload__column').removeChild(lastChild);
+    this.ings.pop();
+  }
+
+  // Handler method to remove an ingredinet input field from the Dom
+  addHandlerRemoveIng(handler) {
+    this._parentEl.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn_remove');
+      if (!btn) return;
+      const children = document.querySelector('.upload__column').children;
+      const lastChild = children[children.length - 1];
+      handler(lastChild);
+    });
+  }
+
+  // Markup for upload recipe form
+  _generateMarkup() {
+    return `
+        <div class="upload__column-data">
+          <h3 class="upload__heading-data">Recipe data</h3>
+          <label>Title</label>
+          <input required name="title" type="text" minlength="3"/>
+          <label>URL</label>
+          <input required name="sourceUrl" type="text" minlength="5"/>
+          <label>Image URL</label>
+          <input required name="image" type="text" minlength="4"/>
+          <label>Publisher</label>
+          <input required name="publisher" type="text" minlength="4"/>
+          <label>Prep time</label>
+          <input required name="cookingTime" type="number" />
+          <label>Servings</label>
+          <input required name="servings" type="number" />
+        </div>
+
+        <div class="upload__column">
+          <h3 class="upload__heading">Ingredients</h3>
+          <div class="upload__column--ing">
+            <label>Ingredient 1</label>
+            <input
+              type="number"
+              step=".01"
+              name="ingredient-1-1"
+              placeholder="Quantity"
+            />
+            <input
+              type="text"
+              name="ingredient-1-2"
+              placeholder="Unit"
+            />
+            <input
+              type="text"
+              required
+              name="ingredient-1-3"
+              placeholder="Description"
+            />
+            <button type="button" data-title="Add ingredient" class="btn_ing btn_add">
+              <svg class="">
+                <use href="${icons}#icon-plus"></use>
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        <button class="btn upload__btn">
+          <svg>
+            <use href="${icons}#icon-upload-cloud"></use>
+          </svg>
+          <span>Upload</span>
+        </button>
+    `;
+  }
+
+  // Markup for adding an ingredient
+  _addIngMarkup() {
+    return `
+          <div class="upload__column--ing">
+            <label>Ingredient ${this.ings.length + 1}</label>
+            <input
+              type="number"
+              step=".01"
+              name="ingredient-${this.ings.length + 1}-1"
+              placeholder="Quantity"
+            />
+            <input
+              type="text"
+              name="ingredient-${this.ings.length + 1}-2"
+              placeholder="Unit"
+            />
+            <input
+              type="text"
+              required
+              name="ingredient-${this.ings.length + 1}-3"
+              placeholder="Description"
+            />
+            <button type="button" data-title="Remove ingredient" class="btn_ing btn_remove">
+              <svg class="">
+                <use href="${icons}#icon-minus"></use>
+              </svg>
+            </button>
+          </div>
+    `;
+  }
+}
+
+export default new AddRecipeView();
